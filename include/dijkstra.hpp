6481b197d47f7ea1d19fe429d611db8f520a7412
Kazuki Komatsu <enjouzensyou.boinc@gmail.com>
2015-05-31 01:51:18 +0900

    fix dijkstra.hpp

--- include/dijkstra.hpp
+++ include/dijkstra.hpp
@@ -3,8 +3,11 @@
 #include <vector>
 #include <functional>
 #include <iostream>
+#include <cstring>
 
-template <typename S, typename P, typename GS>
+using namespace std;
+
+template <typename S, typename GS>
 struct Dijkstra
 {
     struct Hash { size_t operator()(S const& s) const { return s.hash(); } };
@@ -21,24 +24,102 @@ struct Dijkstra
         while(!pq.empty()){
             S s = pq.top(); pq.pop();
             auto g = ss.find(s);
-            if(g != ss.end()) { if(*g > s) continue; else ss.erase(g); }
+            if(g != ss.end()) continue;
+            if(s.isEnd()) return s;
             ss.insert(s);
-            fn.iterP(s, [&](P const & p){ pq.push(p.apply(s)); });
+            fn.iterP(s, [&](S const & t){ if(ss.find(t) == ss.end()) { pq.push(t); } });
         }
 
-        auto l = &*(ss.begin());
-        for(auto& e: ss)
-            if(e > *l)
-                l = &e;
+        S s{};
+        return s;
+    }
+};
 
-        return *l;
+
+template <typename S, typename GS>
+struct Dijkstra1D
+{
+    Dijkstra1D(GS f, size_t n) : fn(f), N(n) {}
+    GS fn;
+    size_t N;
+
+    S solve(S init)
+    {
+        std::priority_queue<S, std::vector<S>, std::greater<S>> q;
+        vector<bool> ss(N, false);
+
+        q.push(init);
+        while(!q.empty()){
+            S s = q.top(); q.pop();
+            if(ss[s.i]) continue;
+            if(s.isEnd()) return s;
+            ss[s.i] = 1;
+            fn.iterP(s, [&](S const & t){ if(!ss[t.i]) { q.push(t); } });
+        }
+
+        S s{};
+        return s;
+    }
+};
+
+
+template <typename S, typename GS>
+struct Dijkstra2D
+{
+    Dijkstra2D(GS f, size_t r, size_t c) : fn(f), R(r), C(c) {}
+    GS fn;
+    size_t R, C;
+
+    S solve(S init)
+    {
+        std::priority_queue<S, std::vector<S>, std::greater<S>> q;
+        vector<vector<bool>> m(R, vector<bool>(C, false));
+
+        q.push(init);
+        while(!q.empty()){
+            S s = q.top(); q.pop();
+            if(m[s.i][s.j]) continue;
+            if(s.isEnd()) return s;
+            m[s.i][s.j] = 1;
+            fn.iterP(s, [&](S const & t){ if(!m[t.i][t.j]) { q.push(t); } });
+        }
+
+        S s{};
+        return s;
+    }
+};
+
+
+template <typename S, typename GS>
+struct Dijkstra3D
+{
+    Dijkstra3D(GS f, size_t a, size_t b, size_t c) : fn(f), A(a), B(b), C(c) {}
+    GS fn;
+    size_t A, B, C;
+
+    S solve(S init)
+    {
+        std::priority_queue<S, std::vector<S>, std::greater<S>> q;
+        vector<vector<vector<bool>>> m(A, vector<vector<bool>>(B, vector<bool>(C, false)));
+
+        q.push(init);
+        while(!q.empty()){
+            S s = q.top(); q.pop();
+            if(m[s.i][s.j][s.k]) continue;
+            if(s.isEnd()) return s;
+            m[s.i][s.j][s.k] = 1;
+            fn.iterP(s, [&](S const & t){ if(!m[t.i][t.j][t.k]) { q.push(t); } });
+        }
+
+        S s{};
+        return s;
     }
 };
 
 
 /** example Project Euler 18
 using uint = unsigned int;
-std::vector<std::vector<uint>> v = {{75},
+std::vector<std::vector<int>> v = {{75},
 {95,64},
 {17,47,82},
 {18,35,87,10},
@@ -56,48 +137,63 @@ std::vector<std::vector<uint>> v = {{75},
 
 // 状態を表す型
 struct S {
-    uint i, j, c;
+    int i, j, c;
 
-    // 評価値の高さを比較
-    bool operator>(S const & e) const { return c > e.c; }
+    // コストの大きさを比較
+    bool operator>(S const & e) const { return (i*100 - c) > (e.i*100 - e.c); }
 
     // 状態が等しいか比較
     bool operator==(S const & e) const { return i == e.i && j == e.j; }
 
     // ハッシュ関数
     size_t hash() const { return i + j; }
-};
 
-// パス(枝)を表す型
-struct P {
-    uint i, j, c;
-
-    // ある状態から、このパスを通った次の状態への遷移
-    S apply(S const & s) const { return {i, j, s.c + c}; }
+    // 終了判定
+    bool isEnd() const { return i == v.size()-1; }
 };
 
 // 全体の状態
 struct GS
 {
-    // ある状態から通ることができるパスを列挙する
+    // ある状態から到達できる次の状態を列挙する
     template <typename Fn>
     void iterP(S & s, Fn fn)
     {
-        if(s.i == v.size()-1)
-            return;
+        auto i = s.i+1, j = s.j;
 
-        auto i = s.i+1,
-             j = s.j;
-        fn({i, j, v[i][j]});
-        fn({i, j+1, v[i][j+1]});
+        if(i == v.size()) return;
+
+        fn({i, j, v[i][j] + s.c});
+        fn({i, j+1, v[i][j+1] + s.c});
+    }
+};
+
+// 2次元バージョン
+struct S2D {
+    int i, j,   // i, j is necessary.
+           c;
+    bool operator>(S2D const & e) const { return (i*100 - c) > (e.i*100 - e.c); }
+    bool isEnd() const { return i == v.size()-1; }
+};
+
+// 2次元バージョン
+struct GS2D
+{
+    template <typename Fn>
+    void iterP(S2D & s, Fn fn)
+    {
+        auto i = s.i+1, j = s.j;
+        if(i == v.size()) return;
+        fn({i, j, v[i][j] + s.c});
+        fn({i, j+1, v[i][j+1] + s.c});
     }
 };
 
 
 int main()
 {
-    GS fn;
-    std::cout << Dijkstra<S, P, GS>(fn).solve({0, 0, v[0][0]}).c << std::endl;
-    return 0;
+    GS fn; GS2D fn2D;
+    std::cout << Dijkstra<S, GS>(fn).solve({0, 0, v[0][0]}).c << std::endl;
+    std::cout << Dijkstra2D<S2D, GS2D>(fn2D, 15, 15).solve({0, 0, v[0][0]}).c << std::endl;
 }
 */
